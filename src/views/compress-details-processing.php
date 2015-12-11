34f92844ac7afe24666c60653cc2ecbbbb0e8b16
Jippe Holwerda <jippeholwerda@voormedia.com>
2015-12-11 19:12:59 +0100

    Do not use shorthand php tags anywhere.

--- src/views/compress-details-processing.php
+++ src/views/compress-details-processing.php
@@ -2,7 +2,7 @@
     <div class="details">
         <span class="icon spinner"></span>
         <span class="message">
-            <strong><?= $compressing ?></strong>
+            <strong><?php echo $compressing ?></strong>
             <span><?php printf(self::translate_escape('%s being compressed'), $compressing == 1 ? 'size' : 'sizes'); ?></span>
         </span>
     </div>
--- src/views/compress-details.php
+++ src/views/compress-details.php
@@ -13,7 +13,7 @@
 
         <?php if ($tiny_metadata->get_compressed_count() > 0 || ($tiny_metadata->get_compressed_count() == 0 && count($uncompressed) == 0)) { ?>
             <span class="message">
-                <strong><?= $tiny_metadata->get_compressed_count() ?></strong>
+                <strong><?php echo $tiny_metadata->get_compressed_count() ?></strong>
                 <span><?php printf(self::translate_escape('%s compressed'), ($tiny_metadata->get_compressed_count() == 1) ? "size" : "sizes") ?></span>
             </span>
             <br/>
@@ -42,44 +42,44 @@
 
         <?php if ($savings["input"] - $savings["output"]) { ?>
             <span class="message">
-                <?= self::translate_escape('Total savings') ?>&nbsp;<?= size_format($savings["input"] - $savings["output"], 1) ?>
+                <?php echo self::translate_escape('Total savings') ?>&nbsp;<?php echo size_format($savings["input"] - $savings["output"], 1) ?>
             </span>
             <br />
         <?php } ?>
 
         <?php if ($error) { ?>
             <span class="message error_message">
-                <?= self::translate_escape('Latest error') . ': '. self::translate_escape($error) ?>
+                <?php echo self::translate_escape('Latest error') . ': '. self::translate_escape($error) ?>
             </span>
             <br/>
         <?php } ?>
 
         <?php if ($tiny_metadata->get_compressed_count() > 0) { ?>
-            <a class="thickbox message" href="#TB_inline?width=700&amp;height=500&amp;inlineId=modal_<?= $tiny_metadata->get_id() ?>">Details</a>
+            <a class="thickbox message" href="#TB_inline?width=700&amp;height=500&amp;inlineId=modal_<?php echo $tiny_metadata->get_id() ?>">Details</a>
         <?php } ?>
     </div>
 
     <?php if (count($uncompressed) > 0) { ?>
-        <button type="button" class="tiny-compress button button-small button-primary" data-id="<?= $tiny_metadata->get_id() ?>">
-            <?= self::translate_escape('Compress') ?>
+        <button type="button" class="tiny-compress button button-small button-primary" data-id="<?php echo $tiny_metadata->get_id() ?>">
+            <?php echo self::translate_escape('Compress') ?>
         </button>
     <?php } ?>
 </div>
 <?php if ($tiny_metadata->get_compressed_count() > 0) { ?>
-    <div class="modal" id="modal_<?= $tiny_metadata->get_id() ?>">
+    <div class="modal" id="modal_<?php echo $tiny_metadata->get_id() ?>">
         <div class="tiny-compression-details">
             <h3><?php printf(self::translate_escape('Compression details for %s'), $tiny_metadata->get_name()) ?></h3>
             <table>
                 <tr>
-                    <th><?= self::translate_escape('Size') ?></th>
-                    <th><?= self::translate_escape('Original size') ?></th>
-                    <th><?= self::translate_escape('Compressed size') ?></th>
-                    <th><?= self::translate_escape('Date') ?></th>
+                    <th><?php echo self::translate_escape('Size') ?></th>
+                    <th><?php echo self::translate_escape('Original size') ?></th>
+                    <th><?php echo self::translate_escape('Compressed size') ?></th>
+                    <th><?php echo self::translate_escape('Date') ?></th>
                 </tr>
                 <?php $i = 0; ?>
                 <?php foreach ($tiny_metadata->get_compressed_sizes() as $size) { ?>
                     <?php $meta = $tiny_metadata->get_value($size); ?>
-                    <tr class="<?= ($i % 2 == 0) ? 'even' : 'odd' ?>">
+                    <tr class="<?php echo ($i % 2 == 0) ? 'even' : 'odd' ?>">
                         <td>
                             <?php
                             if ($size == "0") {
@@ -93,25 +93,25 @@
                                         ?><em>&nbsp;<?php printf(self::translate_escape('(resized to %dx%d)'), $meta['output']['width'], $meta['output']['height']) ?></em><?php
                                     }
                                 } else {
-                                    ?><em>&nbsp;<?= self::translate_escape('(modified after compression)') ?></em><?php
+                                    ?><em>&nbsp;<?php echo self::translate_escape('(modified after compression)') ?></em><?php
                                 }
                             } else {
-                                ?><em>&nbsp;<?= self::translate_escape('(missing)') ?></em><?php
+                                ?><em>&nbsp;<?php echo self::translate_escape('(missing)') ?></em><?php
                             }
                             ?>
                         </td>
-                        <td><?= size_format($meta["input"]["size"], 1) ?></td>
-                        <td><?= size_format($meta["output"]["size"], 1) ?></td>
-                        <td><?= human_time_diff($tiny_metadata->get_end_time($size)) . ' ' . self::translate_escape('ago') ?></td>
+                        <td><?php echo size_format($meta["input"]["size"], 1) ?></td>
+                        <td><?php echo size_format($meta["output"]["size"], 1) ?></td>
+                        <td><?php echo human_time_diff($tiny_metadata->get_end_time($size)) . ' ' . self::translate_escape('ago') ?></td>
                     </tr>
                     <?php $i++; ?>
                 <?php } ?>
                 <?php if ($savings['count'] > 0) { ?>
                 <tfoot>
                     <tr>
-                        <td><?= self::translate_escape('Combined') ?></td>
-                        <td><?= size_format($savings['input'], 1) ?></td>
-                        <td><?= size_format($savings['output'], 1) ?></td>
+                        <td><?php echo self::translate_escape('Combined') ?></td>
+                        <td><?php echo size_format($savings['input'], 1) ?></td>
+                        <td><?php echo size_format($savings['output'], 1) ?></td>
                         <td></td>
                     </tr>
                 </tfoot>
