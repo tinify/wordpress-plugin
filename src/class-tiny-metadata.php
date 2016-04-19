tinify/wordpress-plugin

commit 8f466baa1b334a4214f08912f646905eb251f67b

Refactoring of metadata logic into separte class

src/class-tiny-metadata.php
@@ -24,9 +24,7 @@ class Tiny_Metadata {
 
     private $id;
     private $name;
-    private $values;
-    private $filenames;
-    private $urls;
+    private $images = array();
 
     public function __construct($id, $wp_metadata=null) {
         $this->id = $id;
@@ -35,15 +33,20 @@ class Tiny_Metadata {
             $wp_metadata = wp_get_attachment_metadata($id);
         }
         $this->parse_wp_metadata($wp_metadata);
-        $this->values = get_post_meta($this->id, self::META_KEY, true);
-        if (!is_array($this->values)) {
-            $this->values = array();
+
+        $values = get_post_meta($this->id, self::META_KEY, true);
+        if (!is_array($values)) {
+            $values = array();
+        }
+        foreach ($values as $size => $meta) {
+            if (!isset($this->images[$size])) {
+                $this->images[$size] = new Tiny_Metadata_Image();
+            }
+            $this->images[$size]->meta = $meta;
         }
     }
 
     private function parse_wp_metadata($wp_metadata) {
-        $this->filenames = array();
-        $this->urls = array();
         if (!is_array($wp_metadata)) {
             return;
         }
@@ -59,8 +62,9 @@ class Tiny_Metadata {
 
         $this->name = $path_info['basename'];
 
-        $this->filenames[self::ORIGINAL] = "$path_prefix${path_info['basename']}";
-        $this->urls[self::ORIGINAL] = "$url_prefix${path_info['basename']}";
+        $this->images[self::ORIGINAL] = new Tiny_Metadata_Image(
+            "$path_prefix${path_info['basename']}",
+            "$url_prefix${path_info['basename']}");
 
         $unique_sizes = array();
         if (isset($wp_metadata['sizes']) && is_array($wp_metadata['sizes'])) {
@@ -68,44 +72,44 @@ class Tiny_Metadata {
                 $filename = $info['file'];
 
                 if (!isset($unique_sizes[$filename])) {
-                    $this->filenames[$size] = "$path_prefix$filename";
-                    $this->urls[$size] = "$url_prefix$filename";
-                    $unique_sizes[$filename] = true;
+                    $this->images[$size] = new Tiny_Metadata_Image(
+                        "$path_prefix$filename", "$url_prefix$filename");
                 }
             }
         }
     }
 
+    public function get_image($size=self::ORIGINAL, $create=false) {
+        if (isset($this->images[$size]))
+            return $this->images[$size];
+        elseif ($create)
+            return new Tiny_Metadata_Image();
+        else
+            return null;
+    }
+
     public function update_wp_metadata($wp_metadata) {
-        $tiny_metadata = $this->get_value();
-        if (isset($tiny_metadata) && isset($tiny_metadata['output']) && isset($tiny_metadata['output']['width']) && isset($tiny_metadata['output']['height'])) {
-            $wp_metadata['width'] = $tiny_metadata['output']['width'];
-            $wp_metadata['height'] = $tiny_metadata['output']['height'];
+        $original = $this->get_image();
+        if (is_null($original) || !is_array($original->meta)) {
+            return $wp_metadata;
+        }
+
+        $m = $original->meta;
+        if (isset($m['output']) && isset($m['output']['width']) && isset($m['output']['height'])) {
+            $wp_metadata['width'] = $m['output']['width'];
+            $wp_metadata['height'] = $m['output']['height'];
         }
         return $wp_metadata;
     }
 
     public function update() {
-        update_post_meta($this->id, self::META_KEY, $this->values);
-    }
-
-    public function add_request($size=self::ORIGINAL) {
-        $this->values[$size] = array(
-            'start' => time()
-        );
-    }
-
-    public function add_response($response, $size=self::ORIGINAL) {
-        $response['end'] = time();
-        $this->values[$size] = array_merge($this->values[$size], $response);
-    }
-
-    public function add_exception($exception, $size=self::ORIGINAL) {
-        $this->values[$size] = array(
-            'error'   => $exception->get_error(),
-            'message' => $exception->getMessage(),
-            'timestamp' => time()
-        );
+        $values = array();
+        foreach ($this->images as $size => $image) {
+            if (is_array($image->meta)) {
+                $values[$size] = $image->meta;
+            }
+        }
+        update_post_meta($this->id, self::META_KEY, $values);
     }
 
     public function get_id() {
@@ -124,110 +128,46 @@ class Tiny_Metadata {
         return get_post_mime_type($this->id);
     }
 
-    public function get_filename($size=self::ORIGINAL) {
-        return isset($this->filenames[$size]) ? $this->filenames[$size] : null;
-    }
-
-    public function get_url($size=self::ORIGINAL) {
-        return isset($this->urls[$size]) ? $this->urls[$size] : null;
-    }
-
-    public function get_value($size=self::ORIGINAL) {
-        return isset($this->values[$size]) ? $this->values[$size] : null;
-    }
-
-    public function get_end_time($size=self::ORIGINAL) {
-        $value = $this->get_value($size);
-        if (array_key_exists("end", $value)) {
-            return $value["end"];
-        } else if (array_key_exists("timestamp", $value)) {
-            return $value["timestamp"];
+    public function filter_images($method, $sizes=null) {
+        $selection = array();
+        if (is_null($sizes)) {
+            $sizes = array_keys($this->images);
         }
+        foreach ($sizes as $size) {
+            if (!isset($this->images[$size])) continue;
+            $image = $this->images[$size];
+            if ($image->$method()) {
+                $selection[$size] = $image;
+            }
+        }
+        return $selection;
     }
 
-    public function has_been_compressed($size=self::ORIGINAL) {
-        return isset($this->values[$size]) && isset($this->values[$size]['output']);
-    }
-
-    public function exists($size=self::ORIGINAL) {
-        return file_exists($this->get_filename($size));
-    }
-
-    public function still_exists($size=self::ORIGINAL) {
-        return $this->has_been_compressed($size) && file_exists($this->get_filename($size));
-    }
-
-    public function is_compressed($size=self::ORIGINAL) {
-        return $this->has_been_compressed($size) && $this->still_exists($size)
-            && filesize($this->get_filename($size)) == $this->values[$size]['output']['size'];
-    }
-
-    public function is_compressing($size=self::ORIGINAL) {
-        $meta = $this->values[$size];
-        return isset($meta) && isset($meta['start']) && !isset($meta['output']);
-    }
-
-    public function is_resized($size=self::ORIGINAL) {
-        $meta = $this->values[$size];
-        return isset($meta) && isset($meta['output']) && isset($meta['output']['resized'])
-            && $meta['output']['resized'];
-    }
-
-    public function get_sizes() {
-        return array_keys($this->filenames);
-    }
-
-    public function get_compressed_sizes() {
-        return array_filter(array_keys($this->values), array($this, 'has_been_compressed'));
-    }
-
-    public function get_success_sizes() {
-        return array_filter(array_keys($this->values), array($this, 'is_compressed'));
-    }
-
-    public function get_uncompressed_sizes($active_tinify_sizes) {
-        $sizes = array_intersect($this->get_sizes(), $active_tinify_sizes);
-        $uncompressed = array_diff($sizes, $this->get_success_sizes());
-        return array_filter($uncompressed, array($this, 'exists'));
-    }
-
-    public function get_not_compressed_active_sizes($active_tinify_sizes) {
-        $sizes = array_intersect($this->get_sizes(), $active_tinify_sizes);
-        return array_diff($sizes, $this->get_compressed_sizes());
-    }
-
-    public function get_in_progress_sizes() {
-        return array_filter(array_keys($this->values), array($this, 'is_compressing'));
-    }
-
-    public function get_success_count() {
-        return count($this->get_success_sizes());
-    }
-
-    public function get_in_progress_count() {
-        return count($this->get_in_progress_sizes());
-    }
-
-    public function get_compressed_count() {
-        return count($this->get_compressed_sizes());
-    }
-
-    public function get_missing_count() {
-        return count($this->get_compressed_sizes()) -
-            count(array_filter($this->get_compressed_sizes(), array($this, 'still_exists')));
-    }
-
-    public function get_modified_count() {
-        return count($this->get_compressed_sizes()) - $this->get_success_count() - $this->get_missing_count();
+    public function get_count($methods, $sizes=null) {
+        $stats = array_fill_keys($methods, 0);
+        if (is_null($sizes)) {
+            $sizes = array_keys($this->images);
+        }
+        foreach ($sizes as $size) {
+            if (!isset($this->images[$size])) continue;
+            foreach ($methods as $method) {
+                if ($this->images[$size]->$method()) {
+                    $stats[$method]++;
+                }
+            }
+        }
+        return $stats;
     }
 
     public function get_latest_error() {
         $last_time = null;
         $message = null;
-        foreach ($this->values as $key => $details) {
-            if (isset($details['error']) && isset($details['message']) && ($last_time === null || $last_time < $details['timestamp'])) {
-                $last_time = $details['timestamp'];
-                $message = $details['message'];
+        foreach ($this->images as $size => $image) {
+            if (!is_array($image->meta)) continue;
+            $m = $image->meta;
+            if (isset($m['error']) && isset($m['message']) && ($last_time === null || $last_time < $m['timestamp'])) {
+                $last_time = $m['timestamp'];
+                $message = $m['message'];
             }
         }
         return $message;
@@ -239,17 +179,15 @@ class Tiny_Metadata {
             'output' => 0,
             'count' => 0
         );
-        foreach ($this->values as $key => $details) {
-            if (isset($details['input']) && isset($details['output'])) {
+        foreach ($this->images as $size => $image) {
+            if (!is_array($image->meta)) continue;
+            $m = $image->meta;
+            if (isset($m['input']) && isset($m['output'])) {
                 $result['count']++;
-                $result['input'] += $details['input']['size'];
-                $result['output'] += $details['output']['size'];
+                $result['input'] += $m['input']['size'];
+                $result['output'] += $m['output']['size'];
             }
         }
         return $result;
-    }
-
-    public function is_resizable($size) {
-        return $size === self::ORIGINAL;
     }
 }

src/views/compress-details.php
@@ -1,44 +1,50 @@
-<div class="details-container">
+<?php
+$error = $tiny_metadata->get_latest_error();
+$total = $tiny_metadata->get_count(array('modified', 'missing', 'has_been_compressed', 'compressed'));
+$active = $tiny_metadata->get_count(array('uncompressed', 'never_compressed'), $active_tinify_sizes);
+$savings = $tiny_metadata->get_savings();
+
+?><div class="details-container">
     <div class="details" id="tinify-compress-details">
         <?php if ($tiny_metadata->can_be_compressed()) { ?>
             <?php if ($error) { ?>
                 <span class="icon dashicons dashicons-warning error"></span>
-            <?php } else if ($missing > 0 || $modified > 0) { ?>
+            <?php } else if ($total['missing'] > 0 || $total['modified'] > 0) { ?>
                 <span class="icon dashicons dashicons-yes alert"></span>
-            <?php } else if ($tiny_metadata->get_success_count() > 0 && count($uncompressed) > 0) { ?>
+            <?php } else if ($total['compressed'] > 0 && $active['uncompressed'] > 0) { ?>
                 <span class="icon dashicons dashicons-yes alert"></span>
-            <?php } else if ($tiny_metadata->get_success_count() > 0) { ?>
+            <?php } else if ($total['compressed'] > 0) { ?>
                 <span class="icon dashicons dashicons-yes success"></span>
             <?php } ?>
             <span class="icon spinner hidden"></span>
 
-            <?php if ($tiny_metadata->get_compressed_count() > 0 || ($tiny_metadata->get_compressed_count() == 0 && count($uncompressed) == 0)) { ?>
+            <?php if ($total['has_been_compressed'] > 0 || ($total['has_been_compressed'] == 0 && $active['uncompressed'] == 0)) { ?>
                 <span class="message">
-                    <strong><?php echo $tiny_metadata->get_compressed_count() ?></strong>
+                    <strong><?php echo $total['has_been_compressed'] ?></strong>
                     <span>
-                        <?php echo htmlspecialchars(_n('size compressed', 'sizes compressed', $tiny_metadata->get_compressed_count(), 'tiny-compress-images')) ?>
+                        <?php echo htmlspecialchars(_n('size compressed', 'sizes compressed', $total['has_been_compressed'], 'tiny-compress-images')) ?>
                     </span>
                 </span>
                 <br/>
             <?php } ?>
 
-            <?php if ($not_compressed_active > 0) { ?>
+            <?php if ($active['never_compressed'] > 0) { ?>
                 <span class="message">
-                    <?php echo htmlspecialchars(sprintf(_n('%d size not compressed', '%d sizes not compressed', $not_compressed_active, 'tiny-compress-images'), $not_compressed_active)) ?>
+                    <?php echo htmlspecialchars(sprintf(_n('%d size not compressed', '%d sizes not compressed', $active['never_compressed'], 'tiny-compress-images'), $active['never_compressed'])) ?>
                 </span>
                 <br />
             <?php } ?>
 
-            <?php if ($missing > 0) { ?>
+            <?php if ($total['missing'] > 0) { ?>
                 <span class="message">
-                    <?php echo htmlspecialchars(sprintf(_n('%d file removed', '%d files removed', $missing, 'tiny-compress-images'), $missing)) ?>
+                    <?php echo htmlspecialchars(sprintf(_n('%d file removed', '%d files removed', $total['missing'], 'tiny-compress-images'), $total['missing'])) ?>
                 </span>
                 <br />
             <?php } ?>
 
-            <?php if ($modified > 0) { ?>
+            <?php if ($total['modified'] > 0) { ?>
                 <span class="message">
-                    <?php echo htmlspecialchars(sprintf(_n('%d file modified after compression', '%d files modified after compression', $modified, 'tiny-compress-images'), $modified)) ?>
+                    <?php echo htmlspecialchars(sprintf(_n('%d file modified after compression', '%d files modified after compression', $total['modified'], 'tiny-compress-images'), $total['modified'])) ?>
                 </span>
                 <br />
             <?php } ?>
@@ -57,19 +63,19 @@
                 <br/>
             <?php } ?>
 
-            <?php if ($tiny_metadata->get_compressed_count() > 0) { ?>
+            <?php if ($total['has_been_compressed'] > 0) { ?>
                 <a class="thickbox message" href="#TB_inline?width=700&amp;height=500&amp;inlineId=modal_<?php echo $tiny_metadata->get_id() ?>">Details</a>
             <?php } ?>
         <?php } ?>
     </div>
 
-    <?php if ($tiny_metadata->can_be_compressed() && count($uncompressed) > 0) { ?>
+    <?php if ($tiny_metadata->can_be_compressed() && $active['uncompressed'] > 0) { ?>
         <button type="button" class="tiny-compress button button-small button-primary" data-id="<?php echo $tiny_metadata->get_id() ?>">
             <?php echo esc_html__('Compress', 'tiny-compress-images') ?>
         </button>
     <?php } ?>
 </div>
-<?php if ($tiny_metadata->get_compressed_count() > 0) { ?>
+<?php if ($total['has_been_compressed'] > 0) { ?>
     <div class="modal" id="modal_<?php echo $tiny_metadata->get_id() ?>">
         <div class="tiny-compression-details">
             <h3>
@@ -83,28 +89,26 @@
                     <th><?php esc_html_e('Date', 'tiny-compress-images') ?></th>
                 </tr>
                 <?php $i = 0 ?>
-                <?php foreach ($tiny_metadata->get_compressed_sizes() as $size) { ?>
-                    <?php $meta = $tiny_metadata->get_value($size) ?>
+                <?php foreach ($tiny_metadata->filter_images('has_been_compressed') as $size => $image) {
+                        $meta = $image->meta ? $image->meta : array() ?>
                     <tr class="<?php echo ($i % 2 == 0) ? 'even' : 'odd' ?>">
                         <td>
                             <?php
-                            echo ($size == "0" ? esc_html__('original', 'tiny-compress-images') : $size ) . ' ';
-                            if ($tiny_metadata->still_exists($size)) {
-                                if ($tiny_metadata->is_compressed($size)) {
-                                    if ($tiny_metadata->is_resized($size)) {
-                                        printf('<em>' . esc_html__('(resized to %dx%d)', 'tiny-compress-images') . '</em>', $meta['output']['width'], $meta['output']['height']);
-                                    }
-                                } else {
-                                    echo '<em>' . esc_html__('(modified after compression)', 'tiny-compress-images') . '</em>';
-                                }
-                            } else {
+                            echo ($size === Tiny_Metadata::ORIGINAL ? esc_html__('original', 'tiny-compress-images') : $size ) . ' ';
+                            if ($image->missing()) {
                                 echo '<em>' . esc_html__('(file removed)', 'tiny-compress-images') . '</em>';
+                            } else if ($image->modified()) {
+                                echo '<em>' . esc_html__('(modified after compression)', 'tiny-compress-images') . '</em>';
+                            } else if ($image->resized()) {
+                                printf('<em>' . esc_html__('(resized to %dx%d)', 'tiny-compress-images') . '</em>', $meta['output']['width'], $meta['output']['height']);
+                            } else if (!$image->compressed()) {
+                                echo '<em>' . esc_html__('(unknown state)', 'tiny-compress-images') . '</em>';
                             }
                             ?>
                         </td>
                         <td><?php echo size_format($meta["input"]["size"], 1) ?></td>
                         <td><?php echo size_format($meta["output"]["size"], 1) ?></td>
-                        <td><?php echo human_time_diff($tiny_metadata->get_end_time($size)) . ' ' . esc_html__('ago', 'tiny-compress-images') ?></td>
+                        <td><?php echo human_time_diff($image->end_time($size)) . ' ' . esc_html__('ago', 'tiny-compress-images') ?></td>
                     </tr>
                     <?php $i++ ?>
                 <?php } ?>

src/class-tiny-metadata-image.php
@@ -1,0 +1,117 @@
+<?php
+/*
+* Tiny Compress Images - WordPress plugin.
+* Copyright (C) 2015-2016 Voormedia B.V.
+*
+* This program is free software; you can redistribute it and/or modify it
+* under the terms of the GNU General Public License as published by the Free
+* Software Foundation; either version 2 of the License, or (at your option)
+* any later version.
+*
+* This program is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+* more details.
+*
+* You should have received a copy of the GNU General Public License along
+* with this program; if not, write to the Free Software Foundation, Inc., 51
+* Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+class Tiny_Metadata_Image {
+    public $filename;
+    public $url;
+    public $meta;
+
+    /* Used more than once and not trivial, so we are memoizing these */
+    private $_exists;
+    private $_same_size;
+
+    public function __construct($filename=null, $url=null) {
+        $this->filename = $filename;
+        $this->url = $url;
+    }
+
+    public function end_time() {
+        if (!is_array($this->meta))
+            return null;
+        elseif (isset($this->meta['end']))
+            return $this->meta['end'];
+        elseif (isset($this->meta['timestamp']))
+            return $this->meta['timestamp'];
+        else
+            return null;
+    }
+
+    public function add_request() {
+        $this->meta = array('start' => time());
+    }
+
+    public function add_response($response) {
+        if (is_array($this->meta)) {
+            $this->meta = $response;
+            $this->meta['end'] = time();
+        }
+    }
+
+    public function add_exception($exception) {
+        if (is_array($this->meta)) {
+            $this->meta = array(
+                'error'   => $exception->get_error(),
+                'message' => $exception->getMessage(),
+                'timestamp' => time()
+            );
+        }
+    }
+
+    public function has_been_compressed() {
+        return is_array($this->meta) && isset($this->meta['output']);
+    }
+
+    public function never_compressed() {
+        return !$this->has_been_compressed();
+    }
+
+    private function exists() {
+        if (is_null($this->_exists)) {
+            $this->_exists = $this->filename && file_exists($this->filename);
+        }
+        return $this->_exists;
+    }
+
+    private function same_size() {
+        if (is_null($this->_same_size)) {
+            $this->_same_size = filesize($this->filename) == $this->meta['output']['size'];
+        }
+        return $this->_same_size;
+    }
+
+    public function still_exists() {
+        return $this->has_been_compressed() && $this->exists();
+    }
+
+    public function missing() {
+        return $this->has_been_compressed() && !$this->exists();
+    }
+
+    public function compressed() {
+        return $this->still_exists() && $this->same_size();
+    }
+
+    public function modified() {
+        return $this->still_exists() && !$this->same_size();
+    }
+
+    public function uncompressed() {
+        return $this->exists() && !(isset($this->meta['output']) && $this->same_size());
+    }
+
+    public function in_progress() {
+        return is_array($this->meta) && isset($this->meta['start']) && !isset($this->meta['output']);
+    }
+
+    public function resized() {
+        return is_array($this->meta) && isset($this->meta['output']) && isset($this->meta['output']['resized'])
+            && $this->meta['output']['resized'];
+    }
+}

src/class-tiny-plugin.php
@@ -113,22 +113,22 @@ class Tiny_Plugin extends Tiny_WP_Base {
 
         $compressor = $this->settings->get_compressor();
         $active_tinify_sizes = $this->settings->get_active_tinify_sizes();
-        $uncompressed_sizes = $tiny_metadata->get_uncompressed_sizes($active_tinify_sizes);
+        $uncompressed_images = $tiny_metadata->filter_images('uncompressed', $active_tinify_sizes);
 
-        foreach ($uncompressed_sizes as $uncompressed_size) {
+        foreach ($uncompressed_images as $size => $image) {
             try {
-                $tiny_metadata->add_request($uncompressed_size);
+                $image->add_request();
                 $tiny_metadata->update();
 
-                $resize = $tiny_metadata->is_resizable($uncompressed_size) ? $this->settings->get_resize_options() : false;
+                $resize = ($size == Tiny_Metadata::ORIGINAL) ? $this->settings->get_resize_options() : false;
                 $preserve = count($this->settings->get_preserve_options()) > 0 ? $this->settings->get_preserve_options() : false;
-                $response = $compressor->compress_file($tiny_metadata->get_filename($uncompressed_size), $resize, $preserve);
+                $response = $compressor->compress_file($image->filename, $resize, $preserve);
 
-                $tiny_metadata->add_response($response, $uncompressed_size);
+                $image->add_response($response);
                 $tiny_metadata->update();
                 $success++;
             } catch (Tiny_Exception $e) {
-                $tiny_metadata->add_exception($e, $uncompressed_size);
+                $image->add_exception($e);
                 $tiny_metadata->update();
                 $failed++;
             }
@@ -175,7 +175,7 @@ class Tiny_Plugin extends Tiny_WP_Base {
         if ($json) {
             $result['message'] = $tiny_metadata->get_latest_error();
             $result['status'] = $this->settings->get_status();
-            $result['thumbnail'] = $tiny_metadata->get_url('thumbnail');
+            $result['thumbnail'] = $tiny_metadata->get_image('thumbnail', true)->url;
             echo json_encode($result);
         } else {
             echo $this->render_compress_details($tiny_metadata);
@@ -223,16 +223,9 @@ class Tiny_Plugin extends Tiny_WP_Base {
     }
 
     private function render_compress_details($tiny_metadata) {
-        $active = $this->settings->get_active_tinify_sizes();
-        $uncompressed = $tiny_metadata->get_uncompressed_sizes($active);
-        $not_compressed_active = count($tiny_metadata->get_not_compressed_active_sizes($active));
-        $savings = $tiny_metadata->get_savings();
-        $error = $tiny_metadata->get_latest_error();
-        $missing = $tiny_metadata->get_missing_count();
-        $modified = $tiny_metadata->get_modified_count();
-        $compressing = (count($uncompressed) > 0) ? count($uncompressed) : count($active);
-
-        if ($tiny_metadata->get_in_progress_count() > 0) {
+        $active_tinify_sizes = $this->settings->get_active_tinify_sizes();
+        $in_progress = count($tiny_metadata->filter_images('in_progress'));
+        if ($in_progress > 0) {
             include(dirname(__FILE__) . '/views/compress-details-processing.php');
         } else {
             include(dirname(__FILE__) . '/views/compress-details.php');

src/views/compress-details-processing.php
@@ -1,9 +1,10 @@
-<div class="details-container">
+<?php
+?><div class="details-container">
     <div class="details">
         <span class="icon spinner"></span>
         <span class="message">
-            <strong><?php echo $compressing ?></strong>
-            <span><?php echo _n('size being compressed', 'sizes being compressed', $compressing, 'tiny-compress-images') ?></span>
+            <strong><?php echo $in_progress ?></strong>
+            <span><?php echo _n('size being compressed', 'sizes being compressed', $in_progress, 'tiny-compress-images') ?></span>
         </span>
     </div>
 </div>

commit 943ba2dbbad86283db04ba757c723858743f204d

Prevent recompression of duplicate sizes (again). Commit 8f466baa accidentally removed too much code.

src/class-tiny-metadata.php
@@ -76,6 +76,7 @@ class Tiny_Metadata {
                 $filename = $info['file'];
 
                 if (!isset($unique_sizes[$filename])) {
+                    $unique_sizes[$filename] = true;
                     $this->images[$size] = new Tiny_Metadata_Image(
                         "$path_prefix$filename", "$url_prefix$filename");
                 }

src/views/compress-details.php
@@ -123,7 +123,7 @@ ksort($size_exists);
                             echo '</td>';
                             echo '<td>' . human_time_diff($image->end_time($size)) . ' ' . esc_html__('ago', 'tiny-compress-images') .'</td>';
                         } elseif (!$image->exists()) {
-                            echo '<td colspan=2><em>' . esc_html__('Size is not present', 'tiny-compress-images') . '</em></td>';
+                            echo '<td colspan=2><em>' . esc_html__('Not present or duplicate', 'tiny-compress-images') . '</em></td>';
                         } elseif (isset($size_active[$size])) {
                             echo '<td colspan=2><em>' . esc_html__('Not compressed', 'tiny-compress-images') . '</em></td>';
                         } elseif (isset($size_exists[$size])) {