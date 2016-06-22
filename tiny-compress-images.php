d529f5b58220cce9ff4f2ba00466e894dcf4586f
Rolf Timmermans <rolftimmermans@voormedia.com>
2016-06-22 11:26:03 +0200

    Check style of plugin entry point.

--- tiny-compress-images.php
+++ tiny-compress-images.php
@@ -9,25 +9,25 @@
  * License: GPLv2 or later
  */
 
-require dirname(__FILE__) . '/src/config/tiny-config.php';
-require dirname(__FILE__) . '/src/class-tiny-php.php';
-require dirname(__FILE__) . '/src/class-tiny-wp-base.php';
-require dirname(__FILE__) . '/src/class-tiny-exception.php';
-require dirname(__FILE__) . '/src/class-tiny-compress.php';
-require dirname(__FILE__) . '/src/class-tiny-image-size.php';
-require dirname(__FILE__) . '/src/class-tiny-image.php';
-require dirname(__FILE__) . '/src/class-tiny-settings.php';
-require dirname(__FILE__) . '/src/class-tiny-plugin.php';
-require dirname(__FILE__) . '/src/class-tiny-notices.php';
+require dirname( __FILE__ ) . '/src/config/tiny-config.php';
+require dirname( __FILE__ ) . '/src/class-tiny-php.php';
+require dirname( __FILE__ ) . '/src/class-tiny-wp-base.php';
+require dirname( __FILE__ ) . '/src/class-tiny-exception.php';
+require dirname( __FILE__ ) . '/src/class-tiny-compress.php';
+require dirname( __FILE__ ) . '/src/class-tiny-image-size.php';
+require dirname( __FILE__ ) . '/src/class-tiny-image.php';
+require dirname( __FILE__ ) . '/src/class-tiny-settings.php';
+require dirname( __FILE__ ) . '/src/class-tiny-plugin.php';
+require dirname( __FILE__ ) . '/src/class-tiny-notices.php';
 
-if (Tiny_PHP::client_library_supported()) {
-    require dirname(__FILE__) . '/src/class-tiny-compress-client.php';
+if ( Tiny_PHP::client_library_supported() ) {
+	require dirname( __FILE__ ) . '/src/class-tiny-compress-client.php';
 } else {
-    require dirname(__FILE__) . '/src/class-tiny-compress-fopen.php';
+	require dirname( __FILE__ ) . '/src/class-tiny-compress-fopen.php';
 }
 
 $tiny_plugin = new Tiny_Plugin();
 
-if ( !defined( 'TINY_DEBUG' ) ) {
-    define( 'TINY_DEBUG', null );
+if ( ! defined( 'TINY_DEBUG' ) ) {
+	define( 'TINY_DEBUG', null );
 }
