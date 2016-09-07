76bc5958c9dc937bc16a5970f13b4a366bd30d13
Evert Rotshuizen <evertrotshuizen@voormedia.com>
2016-09-07 17:18:46 +0200

    Fixed failed tests.

--- src/config/tiny-config.php
+++ src/config/tiny-config.php
@@ -6,6 +6,6 @@ if ( ! defined( 'TINY_DEBUG' ) ) {
 
 class Tiny_Config {
 	/* URL is only used by fopen driver. */
-	const URL = 'http://webservice/shrink';
+	const URL = 'https://api.tinify.com/shrink';
 	const MONTHLY_FREE_COMPRESSIONS = 500;
 }
