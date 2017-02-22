5ed7f32de7353e525871d5bfeb6a611885413eba
Simon Wahlstrom <simonwahlstrom@voormedia.com>
2017-02-22 13:18:19 +0100

    Rename wp-include test folder. This will prevent Sucuri and other scanners from identifying 'old installations'.

--- test/helpers/wordpress.php
+++ test/helpers/wordpress.php
@@ -1,7 +1,7 @@
 <?php
 
 define( 'ABSPATH', dirname( dirname( __FILE__ ) ) . '/' );
-define( 'WPINC', 'wp-includes' );
+define( 'WPINC', 'wp-includes-for-tests' );
 
 use org\bovigo\vfs\vfsStream;
 use org\bovigo\vfs\content\LargeFileContent;
