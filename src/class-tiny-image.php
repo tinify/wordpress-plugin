00722acff04a2a9b174fd20c9761932cb5edd800
Rolf Timmermans <rolftimmermans@voormedia.com>
2017-09-15 15:57:10 +0200

    Check for file index.

--- src/class-tiny-image.php
+++ src/class-tiny-image.php
@@ -45,6 +45,12 @@ class Tiny_Image {
 		if ( ! is_array( $this->wp_metadata ) ) {
 			return;
 		}
+		if ( ! isset( $this->wp_metadata['file'] ) ) {
+			/* No file metadata found, this might be another plugin messing with
+			   metadata. Simply ignore this! */
+			return;
+		}
+
 		$path_info = pathinfo( $this->wp_metadata['file'] );
 		$this->name = $path_info['basename'];
 
