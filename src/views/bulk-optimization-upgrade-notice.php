5bd067c1b972b1a0786aab11a01751ba09fd53f2
Simon Wahlstrom <simonwahlstrom@voormedia.com>
2018-11-05 10:43:25 +0100

    Start with upgrade notice for bulk.

---
+++ src/views/bulk-optimization-upgrade-notice.php
@@ -0,0 +1,13 @@
+<?php
+/* I think we should check:
+1: If they have already dismissed the checkbox
+2: If they are on the free plan, if so: */
+?>
+<div class="upgrade-account-notice">
+  <div class="introduction">
+    You are on a <span>x plan</span> with <span>x compressions left</span> this month.
+    Upgrade your account now to stat compressing your entire media library.
+  </div>
+  <button class="button button-primary upgrade-account" data-tiny-action="upgrade-account">Upgrade account</button>
+  <p><a href="#">No thanks, continue anyway</a></p>
+</div>
