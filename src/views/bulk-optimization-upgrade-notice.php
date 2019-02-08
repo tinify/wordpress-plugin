e225b38b0e961cd9072edb65fad2d8b8783921e4
Michiel Verkoijen <michielverkoijen@tinify.com>
2019-02-08 15:45:22 +0100

    Tweaks for the API key account display.

--- src/views/bulk-optimization-upgrade-notice.php
+++ src/views/bulk-optimization-upgrade-notice.php
@@ -1,31 +1,33 @@
 <div class="upgrade-account-notice">
 	<div class="introduction">
-		<p><?php
-		$strong = array(
-			'strong' => array(),
-		);
-		/* translators: %s: number of remaining credits */
-		printf( wp_kses( __(
-			'You are on a <strong>free plan</strong> with <strong>%s compressions left</strong> this month.', // WPCS: Needed for proper translation.
-			'tiny-compress-images'
-		), $strong ), $remaining_credits );
-		?></p>
-		<p><?php
-		echo esc_html__(
-			'Upgrade your account now to compress your entire media library.',
-			'tiny-compress-images'
-		);
-		?></p>
+		<p>
+			<?php
+			$strong = array( 'strong' => array() );
+			/* translators: %s: number of remaining credits */
+			printf( wp_kses( __(
+				'You are on a <strong>free plan</strong> with <strong>%s compressions left</strong> this month.', // WPCS: Needed for proper translation.
+				'tiny-compress-images'
+			), $strong ), $remaining_credits );
+			?>
+		</p>
+		<p>
+			<?php
+			esc_html_e(
+				'Upgrade your account now to compress your entire media library.',
+				'tiny-compress-images'
+			);
+			?>
+		</p>
 	</div>
 	<?php $encoded_email = str_replace( '%20', '%2B', rawurlencode( $email_address ) ); ?>
 	<a href="https://tinypng.com/dashboard/api?type=upgrade&mail=<?php echo $encoded_email; ?>" target="_blank" class="button button-primary button-hero upgrade-account">
-		<?php echo esc_html__( 'Upgrade account', 'tiny-compress-images' ); ?>
-	</a><?php
-	if ( $remaining_credits > 0 ) { ?>
-	<p>
-		<a id="hide-warning" href="#">
-			<?php echo esc_html__( 'No thanks, continue anyway', 'tiny-compress-images' ); ?>
-		</a>
-	</p><?php
-	} ?>
+		<?php esc_html_e( 'Upgrade account', 'tiny-compress-images' ); ?>
+	</a>
+	<?php if ( $remaining_credits > 0 ) { ?>
+		<p>
+			<a id="hide-warning" href="#">
+				<?php esc_html_e( 'No thanks, continue anyway', 'tiny-compress-images' ); ?>
+			</a>
+		</p>
+	<?php } ?>
 </div>
