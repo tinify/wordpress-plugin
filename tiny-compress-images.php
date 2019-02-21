f54b94eca6d98161e9a0d8ac6a388c2320e260a1
Mattijs van Druenen <mattijsvandruenen@voormedia.com>
2019-02-21 11:57:32 +0100

    Bumped version to 3.2.0

--- tiny-compress-images.php
+++ tiny-compress-images.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Compress JPEG & PNG images
  * Description: Speed up your website. Optimize your JPEG and PNG images automatically with TinyPNG.
- * Version: 3.1.0
+ * Version: 3.2.0
  * Author: TinyPNG
  * Author URI: https://tinypng.com
  * Text Domain: tiny-compress-images
