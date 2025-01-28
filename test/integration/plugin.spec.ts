e32bdf2bb9c05cffe051e6784cc0a3397c148819
tijmen <tijmenbruggeman@gmail.com>
2025-01-28 20:02:12 +0100

    Remove the local page

--- test/integration/plugin.spec.ts
+++ test/integration/plugin.spec.ts
@@ -5,20 +5,18 @@ let page: Page;
 test.describe('plugin', () => {
   test.beforeAll(async ({ browser }) => {
     page = await browser.newPage();
+    await page.goto('/wp-admin/plugins.php');
   });
 
-  test('should have title', async ({ page }) => {
-    await page.goto('/wp-admin/plugins.php');
+  test('should have title', async () => {
     await expect(page.getByText('TinyPNG - JPEG, PNG & WebP image compression', { exact: true })).toBeVisible();
   });
 
-  test('includes settings link', async ({ page }) => {
-    await page.goto('/wp-admin/plugins.php');
+  test('includes settings link', async () => {
     await expect(page.getByRole('link', { name: 'Settings', exact: true })).toBeVisible();
   });
 
-  test('includes bulk optimization link', async ({ page }) => {
-    await page.goto('/wp-admin/plugins.php');
+  test('includes bulk optimization link', async () => {
     await expect(page.getByLabel('Main content').getByRole('link', { name: 'Bulk Optimization', exact: true })).toBeVisible();
   });
 });
