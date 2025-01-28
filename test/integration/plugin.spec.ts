38f7087384d7d6de1d5404d379eb78222021bf31
tijmen <tijmenbruggeman@gmail.com>
2025-01-28 19:53:50 +0100

    Single page test

--- test/integration/plugin.spec.ts
+++ test/integration/plugin.spec.ts
@@ -1,6 +1,12 @@
-import { expect, test } from '@playwright/test';
+import { Page, expect, test } from '@playwright/test';
+
+let page: Page;
 
 test.describe('plugin', () => {
+  test.beforeAll(async ({ browser }) => {
+    page = await browser.newPage();
+  });
+
   test('should have title', async ({ page }) => {
     await page.goto('/wp-admin/plugins.php');
     await expect(page.getByText('TinyPNG - JPEG, PNG & WebP image compression', { exact: true })).toBeVisible();
@@ -10,6 +16,7 @@ test.describe('plugin', () => {
     await page.goto('/wp-admin/plugins.php');
     await expect(page.getByRole('link', { name: 'Settings', exact: true })).toBeVisible();
   });
+
   test('includes bulk optimization link', async ({ page }) => {
     await page.goto('/wp-admin/plugins.php');
     await expect(page.getByLabel('Main content').getByRole('link', { name: 'Bulk Optimization', exact: true })).toBeVisible();
