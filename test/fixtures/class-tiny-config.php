6bef6806fd1fa0fbd4e419d9805145ef9df17a5a
tijmen <tijmenbruggeman@gmail.com>
2025-01-29 11:28:58 +0100

    Add filter for adding arguments to as3cf client

--- test/fixtures/class-tiny-config.php
+++ test/fixtures/class-tiny-config.php
@@ -11,3 +11,13 @@ class Tiny_Config {
 	const MONTHLY_FREE_COMPRESSIONS = 500;
 	const META_KEY = 'tiny_compress_images';
 }
+
+/**
+ * Filter for adding arguments to the AS3CF AWS client. 
+ * Required to point the client to the LocalStack instance.
+ */
+add_filter('as3cf_aws_init_client_args', function ($args) {
+	$args['endpoint'] = 'http://host.docker.internal:4566';
+	$args['use_path_style_endpoint'] = true;
+	return $args;
+});
