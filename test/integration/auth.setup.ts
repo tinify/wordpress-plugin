1096bd9297272dc7ba065d36dfb4f267b027500c
tijmen <tijmenbruggeman@gmail.com>
2025-01-31 17:29:57 +0100

    Use id for older versions of wp

--- test/integration/auth.setup.ts
+++ test/integration/auth.setup.ts
@@ -6,8 +6,8 @@ const authFile = path.join(__dirname, './.auth/user.json');
 setup.describe('setup', () => {
   setup('authenticate', async ({ page }) => {
     await page.goto('/wp-login.php');
-    await page.getByLabel('Username or Email Address').fill('admin');
-    await page.getByLabel('Password', { exact: true }).fill('password');
+    await page.fill('#user_login', 'admin');
+    await page.fill('#user_pass', 'password');
 
     await page.getByRole('button', { name: 'Log In' }).click();
 
