302124df2fc6a9a0fc9fb2cd57b2bb0d88220aae
tijmen <tijmenbruggeman@gmail.com>
2025-03-20 10:02:30 +0100

    Add constants

--- src/config/class-tiny-config.php
+++ src/config/class-tiny-config.php
@@ -10,4 +10,16 @@ class Tiny_Config {
 	const KEYS_URL = 'https://api.tinify.com/keys';
 	const MONTHLY_FREE_COMPRESSIONS = 500;
 	const META_KEY = 'tiny_compress_images';
+	const CONVERSION_AVIF = 'image/avif';
+	const CONVERSION_WEBP = 'image/webp';
+
+	/**
+	 * The options for the conversion format.
+	 *
+	 * @var array{string} The options for the conversion format.
+	 */
+	const CONVERSION_FORMAT_OPTIONS = array(
+		self::CONVERSION_AVIF,
+		self::CONVERSION_WEBP,
+	);
 }
