236d9c1bebbb7fd889dd35d3074fdd6c0ea0d444
tijmen <tijmenbruggeman@gmail.com>
2025-04-01 09:52:33 +0200

    Define return types for stream wrapper

--- test/helpers/mock-http-stream-wrapper.php
+++ test/helpers/mock-http-stream-wrapper.php
@@ -25,30 +25,30 @@ class MockHttpStreamWrapper implements IteratorAggregate, ArrayAccess, Countable
 
 	/* IteratorAggregate */
 
-	public function getIterator() {
+	public function getIterator(): Traversable {
 		return new ArrayIterator( $this->data );
 	}
 
 	/* ArrayAccess */
 
-	public function offsetExists( $offset ) {
+	public function offsetExists( $offset ): bool {
 		return array_key_exists( $offset, $this->data );
 	}
 
-	public function offsetGet( $offset ) {
+	public function offsetGet( $offset ): mixed {
 		return $this->data[ $offset ];
 	}
 
-	public function offsetSet( $offset, $value ) {
+	public function offsetSet( $offset, $value ): void {
 		$this->data[ $offset ] = $value;
 	}
 
-	public function offsetUnset( $offset ) {
+	public function offsetUnset( $offset ): void {
 		unset( $this->data[ $offset ] );
 	}
 
 	/* Countable */
-	public function count() {
+	public function count(): int {
 		return count( $this->data );
 	}
 
