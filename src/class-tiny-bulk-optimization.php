41237139961bbf9b0d97d43ef3bcc5566b03df20
Tijmen <tijmenbruggeman@gmail.com>
2025-06-12 14:39:14 +0200

    Fix: When library contains no images, it will show page 1 (#57)
    * When library contains no images, it will show page 1
    
    * Formatting

--- src/class-tiny-bulk-optimization.php
+++ src/class-tiny-bulk-optimization.php
@@ -38,7 +38,9 @@ class Tiny_Bulk_Optimization {
 				$result = self::wpdb_retrieve_images_and_metadata( $last_image_id );
 				$stats = self::populate_optimization_statistics( $settings, $result, $stats );
 				$last_image = end( $result );
-				$last_image_id = $last_image['ID'];
+				if ( isset( $last_image['ID'] ) ) {
+					$last_image_id = $last_image['ID'];
+				}
 			} while ( sizeof( $result ) == self::PAGING_SIZE );
 		} else {
 			$stats = self::populate_optimization_statistics( $settings, $result, $stats );
