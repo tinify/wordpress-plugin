4976ac1d714011111bfeae698600d2579d6b888e
tijmen <tijmenbruggeman@gmail.com>
2025-06-12 13:09:51 +0200

    Use new headless mode

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,6 +24,7 @@ export default defineConfig({
       name: 'chromium',
       use: {
         ...devices['Desktop Chrome'],
+        channel: 'chromium',
         storageState: './test/integration/.auth/user.json',
       },
       fullyParallel: false,
