1ecefe0dc058565922cb7b7ce4a24db3db10a67e
tijmen <tijmenbruggeman@gmail.com>
2025-07-14 15:25:25 +0200

    refactor: reorganize bulk optimization test with clearer structure
    - Reorganize test sections with descriptive comments for better readability
    - Group related mocking operations together (db, images, WordPress functions)
    - Move wpdb mock setup before result data for logical flow
    - Add clear section headers: mock db, create mock image, mock WordPress functions
    - Simplify compressor mock expectations without return value
    - Improve test maintainability and understanding

--- test/unit/TinyCliTest.php
+++ test/unit/TinyCliTest.php
@@ -123,34 +123,17 @@ class Tiny_Cli_Test extends Tiny_TestCase
 
 	public function test_will_compress_all_uncompressed_attachments_if_none_given()
 	{
-		// Define WordPress constants needed for wpdb operations
+		// mock db
 		if (!defined('ARRAY_A')) {
 			define('ARRAY_A', 'ARRAY_A');
 		}
-
-		$this->wp->stub('get_post_mime_type', function ($i) {
-			return 'image/png';
-		});
-
-		// Mock the global wpdb object
 		global $wpdb;
-
-		// Create a mock class that has the methods we need
 		$wpdb = $this->getMockBuilder(stdClass::class)
 			->addMethods(['get_results'])
 			->getMock();
 
 		$wpdb->posts = 'wp_posts';
 		$wpdb->postmeta = 'wp_postmeta';
-
-		$this->wp->stub('wp_get_attachment_metadata', function ($i) {
-			return array(
-				'width' => 1256,
-				'height' => 1256,
-				'file' => '2025/07/test.png',
-				'sizes' => array(),
-			);
-		});
 
 		$mock_results = array(
 			array(
@@ -166,6 +149,11 @@ class Tiny_Cli_Test extends Tiny_TestCase
 				'tiny_meta_value' => ''
 			)
 		);
+
+		$wpdb->method('get_results')
+			->willReturn($mock_results);
+
+		// create mock image
 		$virtual_test_image = array(
 			'path' => '2025/07',
 			'images' => array(
@@ -177,20 +165,32 @@ class Tiny_Cli_Test extends Tiny_TestCase
 		);
 		$this->wp->createImagesFromJSON($virtual_test_image);
 
-		$wpdb->method('get_results')
-			->willReturn($mock_results);
+		// mock wp_get_attachment_metadata
+		$this->wp->stub('wp_get_attachment_metadata', function ($i) {
+			return array(
+				'width' => 1256,
+				'height' => 1256,
+				'file' => '2025/07/test.png',
+				'sizes' => array(),
+			);
+		});
 
+		// mock get_post_mime_type
+		$this->wp->stub('get_post_mime_type', function ($i) {
+			return 'image/png';
+		});
+
+		// mock compressor 
 		$settings = new Tiny_Settings();
 		$mockCompressor = $this->createMock(Tiny_Compress::class);
-
-		$mockCompressor->expects($this->once())
-			->method('compress_file')
-			->willReturn(array('output' => array('size' => 1000)));
-
 		$settings->set_compressor($mockCompressor);
 
-		$command = new Tiny_Command($settings);
+		// create assertion
+		$mockCompressor->expects($this->once())
+			->method('compress_file');
 
+		// invoke test
+		$command = new Tiny_Command($settings);
 		$command->optimize(array(), array());
 	}
 }
