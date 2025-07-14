d59f5f33a97dc9c1d44476d0e609012dc8a09a94
tijmen <tijmenbruggeman@gmail.com>
2025-07-14 13:59:05 +0200

    test: add WP-CLI mock helper for unit tests
    - Add wordpress-cli.php helper file with mock WP_CLI class
    - Include WP-CLI mock in TinyTestCase for all test files
    - Provides mock implementations for WP_CLI methods like log, success, warning
    - Includes WP_CLI\Utils\make_progress_bar mock with MockProgressBar class

---
+++ test/helpers/wordpress-cli.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Mock WP_CLI to be used in php unit tests
+ */
+class WP_CLI
+{
+    public static function log($message) {}
+
+    public static function success($message) {}
+
+    public static function warning($message) {}
+
+    public static function add_command($name, $command) {}
+}
+
+namespace WP_CLI\Utils {
+    class MockProgressBar
+    {
+        public function tick() {}
+
+        public function finish() {}
+    }
+
+    function make_progress_bar($label, $count)
+    {
+        return new MockProgressBar();
+    }
+}
