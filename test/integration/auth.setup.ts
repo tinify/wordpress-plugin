3b9bff9a0dea670cd8404a099a14f6bfc8775c89
tijmen <tijmenbruggeman@gmail.com>
2025-09-03 22:31:39 +0200

    bug: wait for page before setting storage cookie

--- test/integration/auth.setup.ts
+++ test/integration/auth.setup.ts
@@ -1,5 +1,6 @@
 import { test as setup } from '@playwright/test';
 import path from 'path';
+import { BASE_URL } from './utils';
 
 const authFile = path.join(__dirname, './.auth/user.json');
 
@@ -8,8 +9,9 @@ setup.describe('setup', () => {
     await page.goto('/wp-login.php');
     await page.fill('#user_login', 'admin');
     await page.fill('#user_pass', 'password');
-
     await page.getByRole('button', { name: 'Log In' }).click();
+
+    await page.waitForURL(`${BASE_URL}/wp-admin`);
 
     await page.context().storageState({ path: authFile });
   });
