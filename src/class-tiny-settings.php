511009aec920e176b2a42aa385da61569a08b2e3
DAnn2012 <dannunziata@hotmail.it>
2025-11-22 13:22:01 +0100

    Update class-tiny-settings.php

--- src/class-tiny-settings.php
+++ src/class-tiny-settings.php
@@ -718,7 +718,7 @@ class Tiny_Settings extends Tiny_WP_Base {
 		if ( 'background' == $value && $as3cf_local_files_present && $checked ) {
 			echo '<div class="notice notice-warning inline"><p>';
 			echo '<strong>' . esc_html__( 'Warning', 'tiny-compress-images' ) . '</strong> — ';
-			$message = esc_html_e(
+			$message = esc_html__(
 				'For compression to work you will need to configure WP Offload S3 to keep a copy of the images on the server.', // WPCS: Needed for proper translation.
 				'tiny-compress-images'
 			);
