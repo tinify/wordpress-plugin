178d63f8b360ad85e7d3009188f240fe7ed19d8e
tijmen <tijmenbruggeman@gmail.com>
2026-01-05 19:09:29 +0100

    Add initial tests

--- test/unit/TinyDiagnosticsTest.php
+++ test/unit/TinyDiagnosticsTest.php
@@ -8,4 +8,11 @@ class Tiny_Diagnostics_Test extends Tiny_TestCase
 	{
 		parent::set_up();
 	}
+
+	public function test_adds_ajax_action_to_download_diagnostics() {
+		$tiny_settings = new Tiny_Settings();
+		$tiny_diagnostics = new Tiny_Diagnostics($tiny_settings);
+
+		WordPressStubs::assertHook('wp_ajax_tiny_download_diagnostics', array($tiny_diagnostics, 'download_diagnostics'));
+	}
 }
--- test/unit/TinyLoggerTest.php
+++ test/unit/TinyLoggerTest.php
@@ -1,5 +1,7 @@
 <?php
 
+use function PHPUnit\Framework\assertEquals;
+
 require_once dirname(__FILE__) . '/TinyTestCase.php';
 
 class Tiny_Logger_Test extends Tiny_TestCase
@@ -7,5 +9,12 @@ class Tiny_Logger_Test extends Tiny_TestCase
 	public function set_up()
 	{
 		parent::set_up();
+	}
+
+	public function test_logger_always_has_one_instance()
+	{
+		$instance1 = Tiny_Logger::get_instance();
+		$instance2 = Tiny_Logger::get_instance();
+		assertEquals($instance1, $instance2, 'logger should be a singleton');
 	}
 }
