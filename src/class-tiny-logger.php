cf218ee21edf3f0e9adbeaf3d451e1066c8f5866
tijmen <tijmenbruggeman@gmail.com>
2026-01-19 14:17:35 +0100

    format

--- src/class-tiny-logger.php
+++ src/class-tiny-logger.php
@@ -180,7 +180,7 @@ class Tiny_Logger {
 		$context_str = ! empty( $context ) ? ' ' . wp_json_encode( $context ) : '';
 		$log_entry = "[{$timestamp}] [{$level_str}] {$message}{$context_str}" . PHP_EOL;
 
-		error_log($log_entry, 3, $this->log_file_path);
+		error_log( $log_entry, 3, $this->log_file_path );
 	}
 
 	/**
