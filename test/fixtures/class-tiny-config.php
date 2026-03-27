7d3d0b5bdc770eb4ff3e9f74dce25fdc0aae80cd
tijmen <tijmenbruggeman@gmail.com>
2026-03-27 09:35:38 +0100

    remove e2e for s3 offload and dependencies

--- test/fixtures/class-tiny-config.php
+++ test/fixtures/class-tiny-config.php
@@ -3,10 +3,6 @@
 if (! defined('TINY_DEBUG')) {
 	define('TINY_DEBUG', null);
 }
-define('AWS_REGION', getenv('AWS_REGION'));
-define('AWS_ENDPOINT', getenv('AWS_ENDPOINT'));
-define('AWS_ACCESS_KEY_ID', getenv('AWS_ACCESS_KEY_ID'));
-define('AWS_SECRET_ACCESS_KEY', getenv('AWS_SECRET_ACCESS_KEY'));
 
 class Tiny_Config
 {
@@ -16,23 +12,6 @@ class Tiny_Config
 	const MONTHLY_FREE_COMPRESSIONS = 500;
 	const META_KEY = 'tiny_compress_images';
 }
-
-/**
- * Filter for adding arguments to the AS3CF AWS client. 
- * Required to point the client to the LocalStack instance.
- */
-add_filter('as3cf_aws_init_client_args', function ($args) {
-	$args['endpoint'] = AWS_ENDPOINT;
-	$args['use_path_style_endpoint'] = true;
-	$args['region'] = AWS_REGION;
-	return $args;
-});
-
-// Force all S3 URLs to use LocalStack instead of s3.amazonaws.com
-add_filter('as3cf_aws_s3_url_domain', function ($domain, $bucket, $region, $expires, $args) {
-	// Replace any AWS domain with LocalStack
-	return AWS_ENDPOINT . '/' . $bucket;
-}, 10, 5);
 
 // ajax hook to delete all attachments as doing it via UI is flaky
 add_action( 'wp_ajax_clear_media_library', 'clear_media_library' );
