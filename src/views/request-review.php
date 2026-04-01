b703d70d19b4473e3fa2950f44ddc7c2b365e687
tijmen <tijmenbruggeman@gmail.com>
2026-04-01 10:41:00 +0200

    format

--- src/views/request-review.php
+++ src/views/request-review.php
@@ -1,6 +1,6 @@
 <span>
-    <?php esc_html_e( 'Enjoying TinyPNG?', 'tiny-compress-images' ); ?>
+	<?php esc_html_e( 'Enjoying TinyPNG?', 'tiny-compress-images' ); ?>
 </span>
 <a href="https://wordpress.org/support/plugin/tiny-compress-images/reviews/#new-post" target="_blank">
-    <?php esc_html_e( 'Leave a review', 'tiny-compress-images' ); ?>
+	<?php esc_html_e( 'Leave a review', 'tiny-compress-images' ); ?>
 </a>
