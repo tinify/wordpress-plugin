578b82fd90ed53d43b2c5b41ff891aa96dd6d041
tijmen <tijmenbruggeman@gmail.com>
2026-04-29 14:40:30 +0200

    prevent case when filename contains base_dir in path

--- test/unit/TinyPluginTest.php
+++ test/unit/TinyPluginTest.php
@@ -508,7 +508,6 @@ class Tiny_Plugin_Test extends Tiny_TestCase
 
 	public function test_will_copy_original_file_on_backup() {
 		$this->wp->createImage( 37857, '2026/04', 'testfile.png' );
-		$og_file_path = $this->vfs->url() . '/wp-content/uploads/2026/04/testfile.png';
 		$expected_backup = $this->vfs->url() . '/wp-content/uploads/tinify_backup/2026/04/testfile.png';
 
 		$tiny_plugin = new Tiny_Plugin();
@@ -537,7 +536,6 @@ class Tiny_Plugin_Test extends Tiny_TestCase
 
 	public function test_no_backup_when_backup_exists() {
 		$this->wp->createImage( 37857, '2026/04', 'testfile.png' );
-		$og_file_path = $this->vfs->url() . '/wp-content/uploads/2026/04/testfile.png';
 		$expected_backup = $this->vfs->url() . '/wp-content/uploads/tinify_backup/2026/04/testfile.png';
 
 		$tiny_plugin = new Tiny_Plugin();
@@ -564,5 +562,38 @@ class Tiny_Plugin_Test extends Tiny_TestCase
 
 		assertFalse($backup_made, 'expected backup not to be made');
 		assertTrue(file_exists($expected_backup), 'expected backup to exist');
+	}
+
+	/**
+	 * when the attachment file path contains the upload directory name as a path 
+	 * segment, the relative path must be extracted using only the leading basedir prefix
+	 */
+	public function test_backup_preserves_upload_dir_name_in_relative_path() {
+		$this->wp->createImage( 37857, 'wp-content/uploads/2026/04', 'testfile.png' );
+
+		$tiny_plugin = new Tiny_Plugin();
+
+		$ref = new \ReflectionClass($tiny_plugin);
+		$settings_prop = $ref->getProperty('settings');
+		$settings_prop->setAccessible(true);
+		$mock_settings = $this->createMock(Tiny_Settings::class);
+		$mock_settings->method('get_backup_enabled')->willReturn(true);
+		$settings_prop->setValue($tiny_plugin, $mock_settings);
+
+		$this->wp->stub('wp_get_attachment_metadata', function ($i) {
+			return array(
+				'width' => 1256,
+				'height' => 1256,
+				'file' => 'wp-content/uploads/2026/04/testfile.png',
+				'sizes' => array(),
+			);
+		});
+
+		$backup_made = $tiny_plugin->backup_original_image(1);
+
+		assertTrue($backup_made, 'expected backup to be made');
+
+		$expected_backup = $this->vfs->url() . '/wp-content/uploads/tinify_backup/wp-content/uploads/2026/04/testfile.png';
+		assertTrue(file_exists($expected_backup), 'expected backup at path preserving the upload dir segment');
 	}
 }
