a23043316d93c5187872f7915f646e967a3288ee
tijmen <tijmenbruggeman@gmail.com>
2026-05-26 10:52:18 +0200

    phpcs compress-details-processing

--- src/views/compress-details-processing.php
+++ src/views/compress-details-processing.php
@@ -1,4 +1,11 @@
-<div class="details-container" data-status="compressing" data-id="<?php echo $tiny_image->get_id(); ?>">
+<?php
+/**
+ * Loading indicator on media overview page
+ *
+ * @var Tiny_image  $tiny_image
+ */
+?>
+<div class="details-container" data-status="compressing" data-id="<?php echo esc_attr( $tiny_image->get_id() ); ?>">
 	<div class="details">
 		<span class="icon spinner"></span>
 		<span class="message">
