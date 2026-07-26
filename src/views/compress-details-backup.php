c77f3ead33aad82ee786286be9469c519758cd8f
tijmen <tijmenbruggeman@gmail.com>
2026-07-26 22:07:47 +0200

    refactor(backup): improve restore dialog UX and markup structure
    Clearer dialog title/body split, added spinner, descriptive
    confirmation text, and fixed HTML nesting.

--- src/views/compress-details-backup.php
+++ src/views/compress-details-backup.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Details on the backup of the image
  *
@@ -14,22 +15,27 @@ if ( $backup_enabled ) {
 	$backup   = $tiny_image->get_backup();
 	$modal_id = 'modal_' . absint( $tiny_image->get_id() ) . '_backup';
 	?>
-	<p>
-		<?php if ( $backup ) { ?>
+	<?php if ( $backup ) { ?>
+		<p><?php echo esc_html_e('The original upload has been backed up.', 'tiny-compress-images'); ?></p>
+			
+		<p>
 			<a href="<?php echo esc_attr( $backup ); ?>" target="_blank">
-				<?php esc_html_e( 'View Uncompressed' ); ?>
+				<?php esc_html_e( 'View uncompressed file' ); ?>
 			</a>
+			&nbsp;
 			<a class="button button-small" href="#" data-dialog-id="<?php echo esc_attr( $modal_id ); ?>" data-id="<?php echo absint( $tiny_image->get_id() ); ?>">
 				<?php esc_html_e( 'Restore Backup', 'tiny-compress-images' ); ?>
 			</a>
 			<dialog id="<?php echo esc_attr( $modal_id ); ?>" class="tiny-dialog">
-				<p><?php esc_html_e( 'Are you sure you want to restore the original uncompressed image?', 'tiny-compress-images' ); ?></p>
-				
+				<strong class="tiny-dialog-title"><?php esc_html_e( 'Are you sure you want to restore the original uncompressed image?', 'tiny-compress-images' ); ?></strong>
+				<p><?php esc_html_e( 'This action will replace all the compressed images with uncompressed images', 'tiny-compress-images'); ?></p>
+
 				<div class="tiny-dialog-actions">
-					<button value="close" commandfor="<?php echo esc_attr( $modal_id ); ?>" command="close">
+					<span class="spinner"></span>
+					<button value="cancel" commandfor="<?php echo esc_attr( $modal_id ); ?>" command="close" class="button">
 						<?php esc_html_e( 'Cancel', 'tiny-compress-images' ); ?>
 					</button>
-					<button value="submit" commandfor="<?php echo esc_attr( $modal_id ); ?>" command="close" class="button button-primary" autofocus>
+					<button value="submit" commandfor="<?php echo esc_attr( $modal_id ); ?>" command="request-close" class="button button-primary" autofocus>
 						<?php esc_html_e( 'Yes, Restore', 'tiny-compress-images' ); ?>
 					</button>
 				</div>
@@ -39,5 +45,5 @@ if ( $backup_enabled ) {
 				<?php esc_html_e( 'No backup available', 'tiny-compress-images' ); ?>
 			</span>
 		<?php } ?>
-	</p>
-<?php } ?>
+		</p>
+	<?php } ?>
