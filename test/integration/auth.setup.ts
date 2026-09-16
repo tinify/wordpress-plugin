1384207ade55a11e5fedba03f56dc6c4104aabc4
tijmen <tijmenbruggeman@gmail.com>
2026-09-16 16:58:42 +0200

    wait for focus

--- test/integration/auth.setup.ts
+++ test/integration/auth.setup.ts
@@ -1,4 +1,4 @@
-import { test as setup } from '@playwright/test';
+import { test as setup, expect } from '@playwright/test';
 import path from 'path';
 
 const authFile = path.join(__dirname, './.auth/user.json');
@@ -6,8 +6,11 @@ const authFile = path.join(__dirname, './.auth/user.json');
 setup.describe('setup', () => {
   setup('authenticate', async ({ page }) => {
     await page.goto('/wp-login.php');
+    await expect(page.locator('#user_login')).toBeFocused();
     await page.fill('#user_login', 'admin');
     await page.fill('#user_pass', 'password');
+    await expect(page.locator('#user_login')).toHaveValue('admin');
+    await expect(page.locator('#user_pass')).toHaveValue('password');
 
     await Promise.all([
       page.waitForURL('**/wp-admin/**', { timeout: 15000 }),
